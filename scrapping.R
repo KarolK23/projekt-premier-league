6ff7a16aed646113bd0474d5f71774dbab570721
Karol <karol_kowal12@wp.pl>
2023-11-09 16:14:33 +0100

    Dodałem więcej statystyl

--- scrapping.R
+++ scrapping.R
@@ -19,8 +19,17 @@ Expected.goals.allowed <- c()
 Expected.goals.difference <- c()
 Expected.goals.difference.per.90.minutes <- c()
 Attendance <- c()
+Assists <- c()
+Penalty.kicks.made <- c()
+Penalty.kicks.attempted <- c()
+Yellow.cards <- c()
+Red.cards <- c()
+Progressive.carries <- c()
+Progressive.passes <- c()
+Squad2 <- c()
 Season <- c()
-
+#link <- "https://fbref.com/en/comps/9/2018-2019/2018-2019-Premier-League-Stats"
+#page <- read_html(link)
 for (i in 0:5){
   rok <- 2017+i
   link <- paste("https://fbref.com/en/comps/9/",rok,"-",rok+1,"/",rok,"-",rok+1,"-Premier-League-Stats", sep="")
@@ -33,7 +42,6 @@ Rk <- c(Rk,Rkx)
 Squadx <- page %>% html_nodes(".force_mobilize .left:nth-child(2)") %>% html_text()
 Squadx <- Squadx[-(21:40)]
 Squad <- c(Squad, Squadx)
-%>% 
 
 Matches.playedx <- page %>% html_nodes(".force_mobilize .left+ .right") %>% html_text()  
 Matches.playedx <- Matches.playedx[-(21:40)]
@@ -92,12 +100,60 @@ Attendancex <- page %>% html_nodes(".force_mobilize .right:nth-child(16)") %>% h
 Attendancex <- Attendancex[-(21:40)]
 Attendance <- c(Attendance, Attendancex)
 
+#Dodatkowe
+
+#Tutaj drużyny są podane alfabetycznie więc trzeba to jakoś dopasować
+
+Assistsx <- page %>% html_nodes("#stats_squads_standard_for .right:nth-child(10)") %>% html_text()
+Assists <- c(Assists, Assistsx)
+
+Penalty.kicks.madex <- page %>% html_nodes("#stats_squads_standard_for .right:nth-child(13)") %>% html_text()
+Penalty.kicks.made <- c(Penalty.kicks.made, Penalty.kicks.madex)
+
+Penalty.kicks.attemptedx <- page %>% html_nodes("#stats_squads_standard_for .right:nth-child(14)") %>% html_text()
+Penalty.kicks.attempted <- c(Penalty.kicks.attempted, Penalty.kicks.attemptedx)
+
+if (rok!=2017){
+  Yellow.cardsx <- page %>% html_nodes("#stats_squads_standard_for .right:nth-child(15)") %>% html_text()
+  Yellow.cards <- c(Yellow.cards, Yellow.cardsx)
+  
+  Red.cardsx <- page %>% html_nodes("#stats_squads_standard_for .right:nth-child(16)") %>% html_text()
+  Red.cards <- c(Red.cards, Red.cardsx) 
+} else #na stronie fbref są braki dla sezonu 2017/2018 więc pobieramy dane stąd 
+{
+  Yellow.cardsx <- read_html("https://www.espn.com/soccer/stats/_/league/ENG.1/season/2017/view/discipline/english-premier-league") %>% 
+    html_nodes(".Table__TD:nth-child(4) .tar") %>% html_text()
+  Yellow.cards <- c(Yellow.cards, Yellow.cardsx)
+  
+  Red.cardsx <- read_html("https://www.espn.com/soccer/stats/_/league/ENG.1/season/2017/view/discipline/english-premier-league") %>% 
+    html_nodes(".Table__TD:nth-child(5) .tar") %>% html_text()
+  Red.cards <- c(Red.cards, Red.cardsx)
+}
+
+Progressive.carriesx <- page %>% html_nodes("#stats_squads_standard_for .right:nth-child(21)") %>% html_text()
+Progressive.carries <- c(Progressive.carries, Progressive.carriesx)
+
+Progressive.passesx <- page %>% html_nodes("#stats_squads_standard_for .right:nth-child(22)") %>% html_text()
+Progressive.passes <- c(Progressive.passes, Progressive.passesx)
+
+Squad2x <- page %>% html_nodes("#stats_squads_standard_for .left") %>% html_text()
+Squad2 <- c(Squad2, Squad2x)
+
 S <- paste(as.character(rok),"/",as.character(rok+1), sep="")
 Seasonx <- rep(S, times=20)
 Season <- c(Season, Seasonx)
 }
 
-df <- data.frame(Rk, Squad, Matches.played, Wins, Draws, Loses, Goals.for, Goals.against, Goal.difference, Points, Points.per.match, Expected.goals, Expected.goals.allowed, Expected.goals.difference,Expected.goals.difference.per.90.minutes, Attendance, "Season"=rev(Season))
-df
+df1 <- data.frame(Rk, Squad, Matches.played, Wins, Draws, Loses, Goals.for, 
+                 Goals.against, Goal.difference, Points, Points.per.match, 
+                 Expected.goals, Expected.goals.allowed, Expected.goals.difference,
+                 Expected.goals.difference.per.90.minutes, Attendance, "Season"=rev(Season))
+df2 <- data.frame("Squad"=Squad2, Assists, Penalty.kicks.attempted, Penalty.kicks.made, Yellow.cards,
+                  Red.cards, Progressive.carries, Progressive.passes, "Season"=rev(Season))
 
-write.csv(df, "C:\\Premier League Project\\dane.csv", row.names = FALSE)
+
+df <- merge(df1, df2, by = c("Squad","Season"))
+df %>% head()
+df_order <- df %>% arrange(desc(Season), Rk)
+
+write.csv(df_order, "C:\\Premier League Project\\dane.csv", row.names = FALSE)
